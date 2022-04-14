42972d8968616497441e4ca938d8a03f18e7b806
jared <jared@mysmartbeat.com>
2022-04-13 22:46:36 -0600

    dialog kind of working not really

--- Assets/Scripts/InkDialog.cs
+++ Assets/Scripts/InkDialog.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using Ink.Runtime;
 
 public class InkDialog : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI dialogText;
-    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Button buttonPrefab;
     [SerializeField] Color selectedColor;
     public int lettersPerSecond;
 
@@ -16,8 +17,8 @@ public class InkDialog : MonoBehaviour
     [SerializeField] GameObject answerBox; // need this to add answer buttons to dialog
     public Story story;
     public static event Action<Story> OnCreateStory;
-    [SerializeField] private TextAsset inkJSONAsset = null; // ink dialog tree
 
+    [SerializeField] private DialogAsset dialogAsset; // ink dialog tree 
     /*
     1. create story. 
     2. create callback to start story
@@ -26,56 +27,58 @@ public class InkDialog : MonoBehaviour
     5. after selection destroy choices.
     */
 
-    // [SerializeField] Button buttonPrefab;
 
-    void StartStory()
+    public void StartStory()
     {
-        story = new Story(inkJSONAsset.text);
+        story = new Story(dialogAsset.value.text);
         if (OnCreateStory != null) OnCreateStory(story); // does something. instantiates story?
-        // RefreshView();
+        RefreshView();
     }
 
-    // // This is the main function called every time the story changes. It does a few things:
-    // // Destroys all the old content and choices.
-    // // Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
+    // This is the main function called every time the story changes. It does a few things:
+    // Destroys all the old content and choices.
+    // Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
     void RefreshView()
     {
         // Remove all the UI on screen
-        // RemoveChildren();
+        RemoveChildren();
         // 
         // Read all the content until we can't continue any more
         while (story.canContinue)
         {
             // Continue gets the next line of the story
             string text = story.Continue();
+            Debug.Log(text);
             // This removes any white space from the text.
             text = text.Trim();
             // Display the text on screen!
-            // CreateContentView(text);
+            StartCoroutine(CreateContentView(text));
         }
-        // 
         // Display all the choices, if there are any!
         if (story.currentChoices.Count > 0)
         {
             for (int i = 0; i < story.currentChoices.Count; i++)
             {
                 Choice choice = story.currentChoices[i];
-                // Button button = CreateChoiceView(choice.text.Trim());
+                Button button = CreateChoiceView(choice.text.Trim());
                 // Tell the button what to do when we press it
-                // button.onClick.AddListener(delegate
-                // {
-                // OnClickChoiceButton(choice);
-                // });
+                button.onClick.AddListener(delegate
+                {
+                    OnClickChoiceButton(choice);
+                });
             }
         }
         // If we've read all the content and there's no choices, the story is finished!
         else
         {
-            // Button choice = CreateChoiceView("Leave");
-            // choice.onClick.AddListener(delegate
-            // {
+            Button choice = CreateChoiceView("Leave");
+            choice.onClick.AddListener(delegate
+            {
+                RemoveChildren();
+                dialogAsset.value = null;
+                this.gameObject.SetActive(false);
             // TODO: leave dialog
-            // });
+            });
         }
     }
 
@@ -84,43 +87,40 @@ public class InkDialog : MonoBehaviour
     {
         story.ChooseChoiceIndex(choice.index);
         RefreshView();
+        //TODO: store the choices and pass back to a function that updates server
     }
 
     // // Creates a textbox showing the the line of text
-    void CreateContentView(string text)
+    IEnumerator CreateContentView(string text)
     {
-        StartCoroutine(TypeDialog(text));
+        yield return StartCoroutine(TypeDialog(text));
     }
 
     // // Creates a button showing the choice text
-    // Button CreateChoiceView(string text)
-    // {
-    // Creates the button from a prefab
-    // Button choice = Instantiate(buttonPrefab) as Button;
-    // choice.transform.SetParent(canvas.transform, false);
-    // Gets the text from the button prefab
-    // Text choiceText = choice.GetComponentInChildren<Text>();
-    // choiceText.text = text;
-    // Make the button expand to fit the text
-    // HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
-    // layoutGroup.childForceExpandHeight = false;
-    // return choice;
-    // }
+    Button CreateChoiceView(string text)
+    {
+        // Creates the button from a prefab
+        Button choice = Instantiate(buttonPrefab) as Button;
+        choice.transform.SetParent(answerBox.transform, false);
+        // Gets the text from the button prefab
+        Text choiceText = choice.GetComponentInChildren<Text>();
+        choiceText.text = text;
+        // Make the button expand to fit the text
+        // HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
+        // layoutGroup.childForceExpandHeight = false;
+        return choice;
+    }
 
-    // // Destroys all the children of this gameobject (all the UI)
-    // void RemoveChildren()
-    // {
-    // int childCount = dialogBox.transform.childCount;
-    // for (int i = childCount - 1; i >= 0; --i)
-    // {
-    // GameObject.Destroy(dialogBox.transform.GetChild(i).gameObject);
-    // }
-    // childCount = actionSelector.transform.childCount;
-    // for (int i = childCount - 1; i >= 0; --i)
-    // {
-    // GameObject.Destroy(actionSelector.transform.GetChild(i).gameObject);
-    // }
-    // }
+    // Destroys all the children and clears text
+    void RemoveChildren()
+    {
+        SetDialog("");
+        int childCount = answerBox.transform.childCount;
+        for (int i = childCount - 1; i >= 0; --i)
+        {
+            GameObject.Destroy(answerBox.transform.GetChild(i).gameObject);
+        }
+    }
     public void SetDialog(string dialog)
     {
         dialogText.text = dialog;
---
+++ Assets/Scripts/Scriptable Objects/DialogAsset.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New TextAsset", menuName = "Game/TextAsset")]
+public class DialogAsset : ScriptableObject
+{
+    public TextAsset value;
+}
