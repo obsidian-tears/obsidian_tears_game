7c381231b0b69f993d98f08f988950f40f9be15f
dogles16 <dogles16@gmail.com>
2022-06-30 17:07:11 -0600

    clay music pre isaac's merge

--- Assets/Scripts/DynamicMusic.cs
+++ Assets/Scripts/DynamicMusic.cs
@@ -9,12 +9,32 @@ public class DynamicMusic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpdateSong();
+        EnterSong();
     }
 
     // Update is called once per frame
-    public void UpdateSong() {
+    public void EnterSong() {
+        source.volume = 0.0f;
         source.clip = song.value;
         source.Play();
+        StartCoroutine(StartFade(source, 2.0f, 1.0f));
+    }
+    // Update is called once per frame
+    public void ExitSong()
+    {
+        source.volume = 1.0f;
+        StartCoroutine(StartFade(source, 1.0f, 0.0f));
+    }
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 }
