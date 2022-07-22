64e10b4c69f294e6c781e6a6b561c8b05fb6fe9f
Isaac Lowe <ilofosho@gmail.com>
2022-07-21 21:56:26 -0600

    Added menu

---
+++ Assets/Scripts/LevelUpMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        Debug.Log("Level up menu enabled");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Level up menu disabled");
+    }
+}
