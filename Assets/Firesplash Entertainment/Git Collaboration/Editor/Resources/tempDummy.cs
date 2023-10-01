0c2596eb5d9afb48fc8673b3e7c1f161437f0ba5
Tiago Loureiro <tiago.loureiro@jumpseller.com>
2023-10-01 09:00:51 +0100

    Adding only for video, will remove after.

---
+++ Assets/Firesplash Entertainment/Git Collaboration/Editor/Resources/tempDummy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Firesplash.GameDevAssets.GitCollab
+{
+    public class tempDummy : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
