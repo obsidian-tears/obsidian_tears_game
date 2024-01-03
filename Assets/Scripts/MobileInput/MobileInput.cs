6b9a0e6cf54a7cb0336b2052e0c1543557d95305
Meme <MemeRiego@users.noreply.github.com>
2024-01-03 16:21:43 -0300

    Mobile Inputs started

--- Assets/Scripts/MobileInput/MobileInput.cs
+++ Assets/Scripts/MobileInput/MobileInput.cs
@@ -1,18 +1,38 @@
+using Opsive.Shared.Input.VirtualControls;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MobileInput : MonoBehaviour
+
+namespace Opsive.Shared.Input
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class MobileInput : MonoBehaviour
     {
+        [SerializeField] private VirtualJoystick m_VirtualJoystick;
+        [SerializeField] private Button m_InteractButton;
+        [SerializeField] private Button m_RunButton;
+        [SerializeField] GameObject _MobileUI;
+
+
+        public bool running;
+
+        void Start()
+        {
+            if (CheckMobile.IsMobile)
+            {
+                m_VirtualJoystick.enabled = true;
+                _MobileUI.SetActive(true);
+            }
+
+
+
+        }
+
+        void Update()
+        {
         
+        }
     }
 }
