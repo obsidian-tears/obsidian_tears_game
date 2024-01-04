fcc70ba70360145fb76aa21625beb15943c6e623
Meme <MemeRiego@users.noreply.github.com>
2024-01-04 11:45:24 -0300

    Update Mobile Input

--- Assets/Scripts/MobileInput/MobileInput.cs
+++ Assets/Scripts/MobileInput/MobileInput.cs
@@ -1,8 +1,12 @@
 using Opsive.Shared.Input.VirtualControls;
+using PixelCrushers.LoveHate;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static Opsive.UltimateInventorySystem.DatabaseNames.DemoInventoryDatabaseNames;
 
 
 namespace Opsive.Shared.Input
@@ -18,21 +22,77 @@ namespace Opsive.Shared.Input
 
         public bool running;
 
+        [SerializeField] private Player _player;
+
+
+
         void Start()
         {
             if (CheckMobile.IsMobile)
             {
                 m_VirtualJoystick.enabled = true;
                 _MobileUI.SetActive(true);
+
+
+                if (m_InteractButton != null) m_InteractButton.onClick.AddListener(OnInteractButtonClicked);
+                if (m_RunButton != null)
+                {
+                    m_RunButton.onClick.AddListener(OnRunButtonPressed);
+                    m_RunButton.onPointerUp.AddListener(OnRunButtonReleased);
+
+                }
+
             }
 
-
-
         }
+
+
+
 
         void Update()
         {
-        
+            Vector3 joystickInput = new Vector3(m_VirtualJoystick.GetAxis("Horizontal"), m_VirtualJoystick.GetAxis("Vertical"), 0f).normalized;
+            Move(joystickInput);
+            Sprint(running);
+
+
         }
+
+
+        private void Move(Vector3 direction)
+        {
+            
+            _player.myRigidbody.MovePosition(transform.position + direction * _player.speed * Time.deltaTime);
+            _player.animator.SetFloat("moveX", direction.x);
+            _player.animator.SetFloat("moveY", direction.y);
+            _player.animator.SetBool("moving", direction.magnitude > 0);
+
+        }
+
+      
+        private void Sprint(bool isSprinting)
+        {
+            
+        }
+
+        private void OnInteractButtonClicked()
+        {
+            
+        }
+
+
+        private void OnRunButtonPressed()
+        {
+            running = true;
+        }
+
+        private void OnRunButtonReleased()
+        {
+            running = false;
+        }
+
+
+
+
     }
 }
