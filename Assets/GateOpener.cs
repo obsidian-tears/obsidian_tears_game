02d37b7ed48103fd14be294e477071f3db09d677
Derek Frogget <fro22003@byui.edu>
2024-01-15 15:25:20 -0700

    GateOpener Script

---
+++ Assets/GateOpener.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.QuestMachine;
+using PixelCrushers;
+using UnityEngine;
+using System;
+
+public class GateOpener : MonoBehaviour, IMessageHandler
+{
+    [SerializeField] Sprite openGateSprite;
+    [SerializeField] BoxCollider2D gateCollider;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        MessageSystem.AddListener(this, "Quest State Changed", "");
+    }
+
+    // Unregister when this GameObject is destroyed.
+    private void OnDestroy()
+    {
+        MessageSystem.RemoveListener(this);
+    }
+
+    // Handle messages from Quest Machine.
+    public void OnMessage(MessageArgs messageArgs)
+    {
+        OnMessage(messageArgs.message, messageArgs.parameter, messageArgs.sender);
+    }
+    
+    private void OnMessage(string message, string parameter, object s)
+    {
+        if (QuestMachine.GetQuestNodeState("TanasArtifacts", "VillageKey2") == QuestNodeState.Active)
+        {
+            Open();
+        }
+    }
+
+    public void Open() {
+        GetComponent<SpriteRenderer>().sprite = openGateSprite;
+        gateCollider.enabled = false;
+    }
+
+}
