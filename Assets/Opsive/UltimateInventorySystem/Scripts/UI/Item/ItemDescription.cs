4f60f04fb654e810b0fa3b004135286fb868b198
Meme <MemeRiego@users.noreply.github.com>
2024-02-07 12:01:29 -0300

    Update GameUI
    Now you can see in the description of the item if it has restrictions of class.

--- Assets/Opsive/UltimateInventorySystem/Scripts/UI/Item/ItemDescription.cs
+++ Assets/Opsive/UltimateInventorySystem/Scripts/UI/Item/ItemDescription.cs
@@ -6,6 +6,7 @@
 
 namespace Opsive.UltimateInventorySystem.UI.Item
 {
+    using Language.Lua;
     using UnityEngine;
     using Text = Opsive.Shared.UI.Text;
 
@@ -16,14 +17,20 @@ namespace Opsive.UltimateInventorySystem.UI.Item
     {
         [Tooltip("The attribute name for the description.")]
         [SerializeField] protected string m_DescriptionAttributeName = "Description";
+        [Tooltip("The attribute Class.")]
+        [SerializeField] protected string m_Class = "Class";
         [Tooltip("The item name.")]
         [SerializeField] protected Text m_ItemNameText;
         [Tooltip("The description text.")]
         [SerializeField] protected Text m_DescriptionText;
+        [Tooltip("The Class text.")]
+        [SerializeField] protected Text m_ClassText;
         [Tooltip("The text displayed when no item is not selected.")]
         [SerializeField] protected string m_NoItemSelectedMessage = "No Item selected";
         [Tooltip("The text displayed when the item does not have a description.")]
         [SerializeField] protected string m_NoDescriptionMessage = "The item does not have a description";
+        [Tooltip("The text displayed when the item does not have a description.")]
+        [SerializeField] protected string m_NoClass = "No Class";
 
         /// <summary>
         /// Draw the description
@@ -37,6 +44,15 @@ namespace Opsive.UltimateInventorySystem.UI.Item
                 m_DescriptionText.text = descriptionValue;
             } else {
                 m_DescriptionText.text = m_NoDescriptionMessage;
+            }
+
+            if ((ItemInfo.Item.TryGetAttributeValue<string>(m_Class, out var classvalue)))
+            {
+                m_ClassText.text = classvalue;
+            }
+            else
+            {
+                m_ClassText.text = m_NoClass;
             }
         }
 
