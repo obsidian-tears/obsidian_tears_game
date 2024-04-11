ba0c2a4950026d60872de970ccfef0da6ebecdb4
Meme <MemeRiego@users.noreply.github.com>
2024-04-11 12:33:14 -0300

    Update ICConnect, showing canvas behind dialogue canvas.

--- Assets/ICConnect.cs
+++ Assets/ICConnect.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class ICConnect : MonoBehaviour
 {
     public static string characterClass;
-    public static string characterUrl = "https://bhuj6-gaaaa-aaaan-qc7ba-cai.raw.icp0.io/?index=2&battle=true"; // 
+    public static string characterUrl; //= "https://bhuj6-gaaaa-aaaan-qc7ba-cai.raw.icp0.io/?index=2&battle=true"; // 
 
     public void InitData(string json)
     {
         JsonResoult characterData = JsonConvert.DeserializeObject<JsonResoult>(json);
         characterClass = characterData.CharacterClass;
-        //characterUrl = characterData.CharacterUrl; // turn off this to test the url and turn on the url of above
+        characterUrl = characterData.CharacterUrl; // turn off this to test the url and turn on the url of above
     }
       
 }
